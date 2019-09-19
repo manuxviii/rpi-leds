df8736ed864e5003b5cc233931ba99edff012901
manuxviii <emmanuel.mesnige@protonmail.com>
2019-09-19 22:44:04 +0200

    test ok, actualising requests params

--- leds/json.py
+++ leds/json.py
@@ -1,9 +1,7 @@
 import re
 import requests
-import httpauth
-
 
 def get_color():
-    r = requests.get(httpauth.url, auth=(httpauth.login, httpauth.passwd))
+    r = requests.get("localhost/color.json")
     str = re.sub("\"", r':', r.text)
     return(str.split(":")[4])
