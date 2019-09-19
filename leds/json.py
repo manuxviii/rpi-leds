053d94171803b8f50f6d238d1d7ae715899121dd
manuxviii <emmanuel.mesnige@protonmail.com>
2019-09-19 23:35:18 +0200

    retry

--- leds/json.py
+++ leds/json.py
@@ -2,6 +2,6 @@ import re
 import requests
 
 def get_color():
-    r = requests.get("http://localhost/color.json")
+    r = requests.get("http://web/color.json")
     str = re.sub("\"", r':', r.text)
     return(str.split(":")[4])
