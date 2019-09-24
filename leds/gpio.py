fa079c7c280ec978e8fc9fb51512f97dbf2c93bb
manuxviii <emmanuel.mesnige@protonmail.com>
2019-09-24 23:03:26 +0200

    commenting

--- leds/gpio.py
+++ leds/gpio.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 
 def gpio_init(pin_red, pin_green, pin_blue):
+    """This function initialisation the gpio functions.
+    It take as parameters the 3 pins used for controlling the leds"""
+
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
@@ -9,6 +12,9 @@ def gpio_init(pin_red, pin_green, pin_blue):
     GPIO.setup(pin_blue, GPIO.OUT)
 
 def pwm_change_cycle(red_pwm, green_pwm, blue_pwm, color):
+    """This function change the pwm cycle.
+    It take an hexadecimal value as parameter and the leds pins"""
+
     red_pwm.ChangeDutyCycle(int(color[1:3], 16)/2.55)
     green_pwm.ChangeDutyCycle(int(color[3:5], 16)/2.55)
     blue_pwm.ChangeDutyCycle(int(color[5:7], 16)/2.55)
--- leds/leds.py
+++ leds/leds.py
@@ -13,19 +13,24 @@ from time import sleep
 
 old_color = ""
 
+# initialisation du gpio
 gpio.gpio_init(PIN_RED, PIN_GREEN, PIN_BLUE)
 
+# creation des signaux pwm
 red_pwm = GPIO.PWM(PIN_RED, FREQUENCE)
 green_pwm = GPIO.PWM(PIN_GREEN, FREQUENCE)
 blue_pwm = GPIO.PWM(PIN_BLUE, FREQUENCE)
 
+# demarrage des signaux pwm. leds eteintes
 red_pwm.start(250/2.55)
 green_pwm.start(250/2.55)
 blue_pwm.start(250/2.55)
 
 while True:
+    # recuperation du fichier json
     colors = json.get_json()
 
+    # si la couleur change, on update les pwm
     if colors["favcolor"] != old_color:
         gpio.pwm_change_cycle(red_pwm, green_pwm, blue_pwm, colors["favcolor"])
 
